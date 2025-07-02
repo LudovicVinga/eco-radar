4629198191bdccd3e8d235d5a27427f47125c635
Ludovic V <vinga.ludovic@gmail.com>
2025-07-02 10:36:31 +0200

    feat: admin edit password implemented

--- src/Controller/Admin/Profile/ProfileController.php
+++ src/Controller/Admin/Profile/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin\Profile;
 
 use App\Entity\User;
+use App\Form\EditPasswordFormType;
 use App\Form\EditProfileFormType;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
@@ -10,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[Route('/admin')]
 final class ProfileController extends AbstractController
@@ -50,4 +51,40 @@ final class ProfileController extends AbstractController
             "form" => $form->createView()
         ]);
     }
+
+    #[Route('/profile/edit-password', name: 'app_admin_profile_edit_password', methods:['GET', 'POST'])]
+    public function editPassword(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $hasher): Response
+    {
+
+        $form = $this->createForm(EditPasswordFormType::class, null);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            /**
+            * @var User
+            */
+            $admin = $this->getUser();
+
+            $data = $form->getData();
+
+            $passwordHashed = $hasher->hashPassword($admin, $data['plainPassword']);
+
+            $admin->setPassword($passwordHashed);
+            $admin->setUpdatedAt(new DateTimeImmutable());
+
+            $entityManager->persist($admin);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre mot de passe a bien été mis à jour.');
+
+            return $this->redirectToRoute('app_admin_profile_index');
+
+        }
+
+        return $this->render('pages/admin/profile/edit_password.html.twig', [
+            "form" => $form->createView()
+        ]);
+    }
+
 }
