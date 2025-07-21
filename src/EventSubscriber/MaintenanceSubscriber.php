fd2606845a21598af59a221ada907f6f4e3c933f
Ludovic V <vinga.ludovic@gmail.com>
2025-07-21 16:41:02 +0200

    fix: anti-scraping-method

--- src/EventSubscriber/MaintenanceSubscriber.php
+++ src/EventSubscriber/MaintenanceSubscriber.php
@@ -2,11 +2,11 @@
 
 namespace App\EventSubscriber;
 
-use Twig\Environment;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Twig\Environment;
 
 class MaintenanceSubscriber implements EventSubscriberInterface
 {
